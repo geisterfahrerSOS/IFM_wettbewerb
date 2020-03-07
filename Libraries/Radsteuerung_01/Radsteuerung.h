909ddba79110ed8e2962ce13ebc63505b9f20d3f
geisterfahrerSOS <61060689+geisterfahrerSOS@users.noreply.github.com>
2020-03-07 18:02:29 +0100

    added Radsteuerung_01 mit radDrehung funktion

---
+++ Libraries/Radsteuerung_01/Radsteuerung.h
@@ -0,0 +1,32 @@
+#ifndef Radsteuerung_h
+#define Radsteuerung_h
+
+#include <Arduino.h>
+#include <radSensor.h>
+
+
+class Radsteuerung
+{
+public:
+    Radsteuerung();
+    Radsteuerung(int motorLinks, int motorRechts, int richtungLinks, int richtungRechts);
+    void geradeFahren(int distanz); // Distanz in mm wird einmal aufgerufen
+    boolean liveGeradeFahren();
+    void radDrehung(int deltaWinkel);
+    void einzelRad(int distanz, int side);
+    boolean liveEinzelRad();
+    void resetMotors();
+private:
+    int _motorLinks;
+    int _motorRechts;
+    int _richtungLinks;
+    int _richtungRechts;
+    int _geradeDistanz;
+    RadSensor rad[2];
+    long _setStep[2]; //bei welchem Step wird die geradeFahren Funktion aufgerufen 
+    long _einzelRad[2]; //bei einem Rad der vergleichswert um die gefahrene Distanz zu überprüfen
+    int _distanzEinzel[2];
+    int _drehDistanz = 0;
+};
+
+#endif
